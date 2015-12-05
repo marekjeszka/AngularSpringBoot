58f5ace44cc8501b3e323061bc60c5fe7a19392b
Marek Jeszka <marek.jeszka@gmail.com>
2015-12-05 16:37:57 +0100

    Deferred result introduced

--- src/main/java/com/jeszka/hello/GreetingController.java
+++ src/main/java/com/jeszka/hello/GreetingController.java
@@ -1,12 +1,14 @@
 package com.jeszka.hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class GreetingController {
@@ -16,13 +18,23 @@ public class GreetingController {
 
     @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping("/greeting")
-    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
+    public @ResponseBody DeferredResult<Greeting> greeting(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("+++ in greeting +++");
+        DeferredResult<Greeting> deferredResult = new DeferredResult<>();
+
+        CompletableFuture.supplyAsync(() -> execute(name))
+             .whenCompleteAsync((result, throwable) -> deferredResult.setResult(result));
+        System.out.println("--- in greeting ---");
+        return deferredResult;
+    }
+
+    private Greeting execute(String name) {
         try {
-            Thread.sleep(4000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println("Greeting ready");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
